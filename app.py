b39cc72336b97a60f7eb808adfd200e7738321ee
pakobeyo-glitch <pakobeyo@gmail.com>
2026-09-01 20:02:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ elif st.session_state.cislo_sceny == 4:
         st.markdown("<h1 style='text-align: center;'>BÁSEŇ</h1>", unsafe_allow_html=True)
         
         # --- SEM SI MÔŽETE NAPÍSAŤ SVOJ ČISTÝ TEXT ---
-        st.swrite("**Tu je pre vás, milí voliči, nejaké povzbudenie pred voľbami:**")
+        st.write("**Tu je pre vás, milí voliči, nejaké povzbudenie pred voľbami:**")
         st.divider()
         st.write("V tých voľbách demokratických ")
         st.write("je predsa čosi novô,")
